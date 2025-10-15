97b1ba27384a90fb0e5694f1509361c20c2a2dad
Mikhail <>
2025-10-15 15:19:48 +0300

    - немного стартовой сцены

---
+++ Geomax_Test/Assets/_Project/Scripts/StartScene/Scene/StartSceneInstaller.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using ZerglingUnityPlugins.ZenjectExtentions.SceneInstallers;
+
+namespace _Project.Scripts.StartScene.Scene
+{
+    public class StartSceneInstaller : SceneInstaller
+    {
+        protected override void OnInstallBindings()
+        {
+        }
+    }
+}
