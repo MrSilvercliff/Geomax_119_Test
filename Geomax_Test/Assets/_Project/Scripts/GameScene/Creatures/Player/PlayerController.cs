68f21ed137b397a536d5ce2c256902d77f7d15f5
Mikhail <>
2025-10-16 12:25:06 +0300

    - фикс неймспейсов

--- Geomax_Test/Assets/_Project/Scripts/GameScene/Creatures/Player/PlayerController.cs
+++ Geomax_Test/Assets/_Project/Scripts/GameScene/Creatures/Player/PlayerController.cs
@@ -1,5 +1,4 @@
 using _Project.Scripts.GameScene.Creatures.Basis;
-using _Project.Scripts.GameScene.Creatures.Player.States;
 using _Project.Scripts.Project.Enums;
 using System.Collections;
 using System.Collections.Generic;
