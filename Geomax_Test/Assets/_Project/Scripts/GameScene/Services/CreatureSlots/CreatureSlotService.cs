bf47a6374bac18b9fe82f83f6fc122f8723c0bd9
Mikhail <>
2025-10-16 20:54:26 +0300

    - добавил методы получения свободных слотов существ в сервис слотов существ

--- Geomax_Test/Assets/_Project/Scripts/GameScene/Services/CreatureSlots/CreatureSlotService.cs
+++ Geomax_Test/Assets/_Project/Scripts/GameScene/Services/CreatureSlots/CreatureSlotService.cs
@@ -1,23 +1,43 @@
+using _Project.Scripts.GameScene.CreatureSlot;
 using System.Threading.Tasks;
 using UnityEngine;
+using Zenject;
 using ZerglingUnityPlugins.Tools.Scripts.Interfaces.ProjectService.AsyncSync;
 
 namespace _Project.Scripts.GameScene.Services.CreatureSlots
 {
     public interface ICreatureSlotService : IProjectService
-    { 
+    {
+        bool TryGetFreePlayerCreatureSlot(out ICreatureSlotController slotController);
+        bool TryGetFreeEnemyCreatureSlot(out ICreatureSlotController slotController);
     }
 
     public class CreatureSlotService : ICreatureSlotService
     {
-        public Task<bool> Init()
+        [Inject] private ICreatureSlotRepository _slotRepository;
+
+        public async Task<bool> Init()
         {
-            return Task.FromResult(true);
+            await _slotRepository.Init();
+            return true;
         }
 
         public bool Flush()
         {
+            _slotRepository.Flush();
             return true;
+        }
+
+        public bool TryGetFreePlayerCreatureSlot(out ICreatureSlotController slotController)
+        {
+            var result = _slotRepository.TryGetFreePlayerCreatureSlot(out slotController);
+            return result;
+        }
+
+        public bool TryGetFreeEnemyCreatureSlot(out ICreatureSlotController slotController)
+        {
+            var result = _slotRepository.TryGetFreeEnemyCreatureSlot(out slotController);
+            return result;
         }
     }
 }
