82a89a4b0691c588c0921233843647fb3aca08cc
Mikhail <>
2025-10-20 19:46:28 +0300

    - сделал создание стейт машин вражеских существ

--- Geomax_Test/Assets/_Project/Scripts/GameScene/Services/StateMachines/GameSceneStateMachineCreator.cs
+++ Geomax_Test/Assets/_Project/Scripts/GameScene/Services/StateMachines/GameSceneStateMachineCreator.cs
@@ -1,3 +1,4 @@
+using _Project.Scripts.GameScene.Creatures.Enemy;
 using _Project.Scripts.GameScene.Creatures.Player;
 using _Project.Scripts.Project.Enums;
 using _Project.Scripts.Project.Services.StateMachines;
@@ -12,6 +13,7 @@ namespace _Project.Scripts.GameScene.Services.StateMachines
     public class GameSceneStateMachineCreator : StateMachineCreator
     {
         [Inject] private PlayerStateMachine.Factory _playerFactory;
+        [Inject] private EnemyCreatureStateMachine.Factory _enemyCreatureFactory;
 
         protected override IStateMachineBase CreateStateMachineProcess(StateMachineType stateMachineType)
         {
@@ -21,6 +23,10 @@ namespace _Project.Scripts.GameScene.Services.StateMachines
             {
                 case StateMachineType.CREATURE_PLAYER:
                     result = _playerFactory.Create();
+                    break;
+
+                case StateMachineType.CREATURE_ENEMY:
+                    result = _enemyCreatureFactory.Create();
                     break;
             }
 
