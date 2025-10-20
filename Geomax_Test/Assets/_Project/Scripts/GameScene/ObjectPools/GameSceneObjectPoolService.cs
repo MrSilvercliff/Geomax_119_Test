9e54cc9accc8ba0ece405dc0275245b80de41d7d
Mikhail <>
2025-10-20 18:18:24 +0300

    - добавил объектный пул в сервис пулов игровой сцены

--- Geomax_Test/Assets/_Project/Scripts/GameScene/ObjectPools/GameSceneObjectPoolService.cs
+++ Geomax_Test/Assets/_Project/Scripts/GameScene/ObjectPools/GameSceneObjectPoolService.cs
@@ -1,5 +1,6 @@
 using _Project.Scripts.GameScene.Creatures.Basis;
 using _Project.Scripts.GameScene.Creatures.Basis.Prefab;
+using _Project.Scripts.GameScene.Creatures.Enemy;
 using _Project.Scripts.GameScene.Creatures.Player;
 using UnityEngine;
 using Zenject;
@@ -10,14 +11,17 @@ namespace _Project.Scripts.GameScene.ObjectPools
     {
         ICreaturePrefabPool CreaturePrefabPool { get; }
         PlayerController.Pool PlayerControllerPool { get; }
+        EnemyCreatureController.Pool EnemyControllerPool { get; }
     }
 
     public class GameSceneObjectPoolService : IGameSceneObjectPoolService
     {
         public ICreaturePrefabPool CreaturePrefabPool => _creaturePrefabPool;
         public PlayerController.Pool PlayerControllerPool => _playerControllerPool;
+        public CreatureController<EnemyCreatureController>.Pool EnemyControllerPool => _enemyControllerPool;
 
         [Inject] private ICreaturePrefabPool _creaturePrefabPool;
         [Inject] private PlayerController.Pool _playerControllerPool;
+        [Inject] private EnemyCreatureController.Pool _enemyControllerPool; 
     }
 }
