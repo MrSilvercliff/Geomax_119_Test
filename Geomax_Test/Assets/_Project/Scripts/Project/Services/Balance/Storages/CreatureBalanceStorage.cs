1fbdce0c809b564331d9ead5b1b5d906fde0c562
Mikhail <>
2025-10-21 19:27:08 +0300

    - добавил возможность взять все баланс модели врагов в баланс сторадже существ

--- Geomax_Test/Assets/_Project/Scripts/Project/Services/Balance/Storages/CreatureBalanceStorage.cs
+++ Geomax_Test/Assets/_Project/Scripts/Project/Services/Balance/Storages/CreatureBalanceStorage.cs
@@ -9,10 +9,20 @@ namespace _Project.Scripts.Project.Services.Balance.Storages
 {
     public interface ICreatureBalanceStorage : IBalanceStorageDictionaryAsyncBase<ICreatureBalanceModel, CreatureBalanceModel>
     { 
+        IReadOnlyList<ICreatureBalanceModel> Enemies { get; }
     }
 
     public class CreatureBalanceStorage : BalanceStorageDictionaryAsyncBase<ICreatureBalanceModel, CreatureBalanceModel>, ICreatureBalanceStorage
     {
+        public IReadOnlyList<ICreatureBalanceModel> Enemies => _enemies;
+
+        private List<ICreatureBalanceModel> _enemies;
+
+        public CreatureBalanceStorage() : base()
+        { 
+            _enemies = new();
+        }
+
         protected override Task<bool> OnInit()
         {
             return Task.FromResult(true);
@@ -20,6 +30,8 @@ namespace _Project.Scripts.Project.Services.Balance.Storages
         
         protected override void OnBalanceModelAdded(ICreatureBalanceModel balanceModel)
         {
+            if (balanceModel.CreatureType == Enums.CreatureType.ENEMY)
+                _enemies.Add(balanceModel);
         }
     }
 }
