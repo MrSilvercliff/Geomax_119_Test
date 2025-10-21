6403d091aa33553dcd04dfac6065427c23d8a970
Mikhail <>
2025-10-21 13:34:28 +0300

    - фикс неймспейсов

--- Geomax_Test/Assets/_Project/Scripts/Project/Animations/MonoBehaviourAnimatorController.cs
+++ Geomax_Test/Assets/_Project/Scripts/Project/Animations/MonoBehaviourAnimatorController.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 using ZerglingUnityPlugins.Tools.Scripts.Interfaces.Sync;
-using static Codice.CM.Common.CmCallContext;
 
 namespace _Project.Scripts.Project.Animations
 {
