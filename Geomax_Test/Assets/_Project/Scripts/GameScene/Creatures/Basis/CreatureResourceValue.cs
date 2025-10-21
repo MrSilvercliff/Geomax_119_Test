e166dbfba15a33fa3bdd4ff8ab7875b5eaaf3ce6
Mikhail <>
2025-10-21 15:47:15 +0300

    - делаю ресурсы существ

---
+++ Geomax_Test/Assets/_Project/Scripts/GameScene/Creatures/Basis/CreatureResourceValue.cs
@@ -0,0 +1,41 @@
+using _Project.Scripts.Project.Enums;
+using _Project.Scripts.Project.Services.Balance.Models;
+using UnityEngine;
+using Zenject;
+
+namespace _Project.Scripts.GameScene.Creatures.Basis
+{
+    public interface ICreatureResourceValue
+    { 
+        CreatureResourceType ResourceType { get; }
+        int MinValue { get; }
+        int MaxValue { get; }
+        int CurrentValue { get; }
+
+        void SetCurrentValue(int newValue);
+    }
+
+    public class CreatureResourceValue : ICreatureResourceValue
+    {
+        public CreatureResourceType ResourceType => _balanceModel.ResourceType;
+        public int MinValue => _balanceModel.MinValue;
+        public int MaxValue => _balanceModel.MaxValue;
+        public int CurrentValue => _currentValue;
+
+        private ICreatureResourceBalanceModel _balanceModel;
+        private int _currentValue;
+
+        public CreatureResourceValue(ICreatureResourceBalanceModel balanceModel)
+        { 
+            _balanceModel = balanceModel;
+            _currentValue = _balanceModel.StartValue;
+        }
+
+        public void SetCurrentValue(int newValue)
+        {
+            _currentValue = newValue;
+        }
+
+        public class Factory : PlaceholderFactory<ICreatureResourceBalanceModel, CreatureResourceValue> { }
+    }
+}
