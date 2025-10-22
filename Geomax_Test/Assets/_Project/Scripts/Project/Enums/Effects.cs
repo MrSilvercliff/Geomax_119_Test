dbdc365c9e10bdf748a78c1783cdb9d6bbdff142
Mikhail <>
2025-10-22 16:59:13 +0300

    - ковыряю енумы для эффектов

--- Geomax_Test/Assets/_Project/Scripts/Project/Enums/Effects.cs
+++ Geomax_Test/Assets/_Project/Scripts/Project/Enums/Effects.cs
@@ -5,34 +5,43 @@ namespace _Project.Scripts.Project.Enums
     public enum EffectTriggerType
     {
         NONE = 0,
-        Attack = 1,
-        Hit = 2,
-        SkillUse = 3,
-        TimerTick = 4,
+        DAMAGE_DEAL = 1,
+        DAMAGE_RECEIVE = 2,
+        SKILL_USE = 3,
+        TIME_TICK = 4,
     }
 
-    public enum EffectDuration
+    public enum EffectDurationType
     { 
         NONE = 0,
-        Permanent = 1,
-        Temporary = 2,
+        PERMANENT = 1,
+        TEMPORARY = 2,
     }
 
     public enum EffectTargetType
     { 
         NONE = 0,
-        Self = 1,
-        AttackTarget = 2,
-        AllOtherSide = 3,
-        All = 4,
+        SELF = 1,
+        ATTACK_TARGET = 2,
+        ALL_OTHER_SIDE = 3,
+        ALL = 4,
     }
 
     public enum EffectType
     { 
         NONE = 0,
-        Stun = 1,
-        Heal = 2,
-        DamageMultiply = 3,
-        BasicAttackCooldownMultiply = 4,
+        MANA_POINTS_REGEN = 1,
+        EFFECT_TRIGGER = 2,
+        STUN = 3,
+        HEAL_BY_MAX_HIT_POINTS = 4,
+        DAMAGE_MULTIPLY = 5,
+        BASIC_ATTACK_COOLDOWN_MULTIPLY = 6,
+    }
+
+    public enum EffectValueType
+    { 
+        NONE = 0,
+        PERCENTAGE = 1,
+        SOLID = 2,
     }
 }
